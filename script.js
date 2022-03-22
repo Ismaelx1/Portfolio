eb3d86b224054f6463186193ee3a76822257dbfe
Ismail Askhatir <spxoki@gmail.com>
2022-03-22 12:46:35 +0100

    day3

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const whiteBtn = document.getElementById('white')
+const cont = document.querySelector('.container')
+const openTxt = document.getElementById('open')
+const openTxt1 = document.getElementById('open1')
+console.log(cont)
+whiteBtn.addEventListener('click', function() {
+   document.body.style.backgroundColor = 'white'
+   openTxt.style.color = 'black'
+   openTxt1.style.color = 'black'
 
+
+
+
+})
